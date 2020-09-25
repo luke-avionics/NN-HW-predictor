4033213acef2084ed15f9f7245d6695f90840537
luke-avionics <sirsargras@gmail.com>
2020-09-25 15:45:40 -0500

    common utilities file added

--- remote_energy_access.py
+++ remote_energy_access.py
@@ -1,17 +1,24 @@
 import paramiko
 import xml.etree.ElementTree as ET
 import re
+import time
+from predictor_utilities import *
 ssh = paramiko.SSHClient()
 ssh.load_system_host_keys()
 ssh.connect('10.129.1.80', username='hy34', password='youhaoran')
 print('connection established')
+
+#take the input from Yonggan's space and convert it 
+
+#modify and transfer the config file
+
+
+start_time=time.time()
 ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command('source /home/hy34/accelergy_luke/timeloop/env/setup-env.bash;\
                                                       cd /home/hy34/accelergy_luke/timeloop-accelergy-exercises/exercises/timeloop/06-mapper-convlayer-eyeriss;\
                                                       /home/hy34/accelergy_luke/timeloop/build/timeloop-mapper prob/VGG02_layer5.yaml arch/components/*.yaml arch/eyeriss_like.yaml constraints/*.yaml mapper/mapper.yaml > /dev/null 2>&1;\
-                                                      cat /home/hy34/accelergy_luke/timeloop-accelergy-exercises/exercises/timeloop/06-mapper-convlayer-eyeriss/timeloop-mapper.stats.txt')
-                                                      
+                                                      cat /home/hy34/accelergy_luke/timeloop-accelergy-exercises/exercises/timeloop/06-mapper-convlayer-eyeriss/timeloop-mapper.stats.txt')    
 exit_code = ssh_stdout.channel.recv_exit_status() 
-
 stdout = []
 for line in ssh_stdout:
     stdout.append(line.strip())
@@ -23,8 +30,9 @@ for line in ssh_stderr:
 # Clean up elements
 ssh.close()
 del ssh, ssh_stdin, ssh_stdout, ssh_stderr
-print(stdout)
-print(stderr)
+
+
+print('Evaluation & transmission takes: ', time.time() - start_time)
 
 for i, line in enumerate(stdout):
     if 'Cycles:' in line:
